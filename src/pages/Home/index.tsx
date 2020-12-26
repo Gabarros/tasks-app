4fe8b42a545f186a708a1026ce5e5d25d0bfe7e0
Gabarros <gabarros17@gmail.com>
2020-12-26 11:29:31 -0300

    created start and stop counter function

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from "react";
 
-export default function Home(){
-  return(
-    <div>
-      Home Page
-    </div>
-  )
+import { Container } from "./styles";
 
-};
+export default function Home() {
+  const [counter, setCounter] = useState(0);
+  const [isCounting, setIsCounting] = useState(false);
+
+  useEffect(() => {
+    let intervalRef = setInterval(counter, 1000);
+
+    function counter() {
+      if (!isCounting) {
+        clearInterval(intervalRef);
+      } else {
+        setCounter(prevCounter => prevCounter + 1);
+      }
+    }
+
+    return () => {
+      clearInterval(intervalRef);
+    };
+  }, [isCounting]);
+
+  return (
+    <Container>
+      <div>Contador: {counter}</div>
+      <div>
+        <button onClick={() => setIsCounting(true)} disabled={isCounting}>
+          Iniciar Contador
+        </button>
+        <button onClick={() => setIsCounting(false)}>Parar Contador</button>
+      </div>
+    </Container>
+  );
+}
