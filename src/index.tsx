a55dd2c2161d9083b3729f676e32b039832c899c
Gabarros <gabarros17@gmail.com>
2020-12-26 09:58:28 -0300

    created base project and routes configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { profile } from "console";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/profile" component={Profile} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
